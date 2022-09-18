3b7ff26dff87a0827a2310ed58b6b24d5dbb725b
David Gomadza <dgomadza@gmail.com>
2022-09-18 02:36:00 +0100

    da

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -26,7 +26,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1,3, 4,5,42,421611],
+  supportedChainIds: [1,3,4,5,42,421611],
 });
 
 // mainnet only
