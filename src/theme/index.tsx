d54559f029ec3ab38162482eb62b4065c494df37
GtpsFinance <welcome@gtps.finance>
2022-09-28 22:03:05 +0100

    Update index.tsx

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -203,7 +203,7 @@ html {
 
 export const ThemedGlobalStyle = createGlobalStyle`
 html {
-  color: rgba(250, 250, 250, 0);
+  color: rgba(250, 250, 250, 89);
   background-color: ${({ theme }) => theme.bg2};
 }
 body {
