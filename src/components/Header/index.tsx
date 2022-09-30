1543a22b258e366c1769e70b31728c5754b854b9
GtpsFinance <welcome@gtps.finance>
2022-09-30 23:37:44 +0100

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -95,12 +95,12 @@ const HeaderLinks = styled(Row)`
 
 const AccountElement = styled.div<{ active: boolean }>`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
   background-color: ${({ theme, active }) => (!active ? theme.bg1 : theme.bg3)};
   border-radius: 0.8rem;
   white-space: nowrap;
-  width: 100%;
+  width: 70%;
   cursor: pointer;
   box-shadow: rgba(0, 0, 0, 0.01) 0px 0px 1px, rgba(0, 0, 0, 0.04) 0px 4px 8px, rgba(0, 0, 0, 0.04) 0px 16px 24px,
     rgba(0, 0, 0, 0.01) 0px 24px 32px;
